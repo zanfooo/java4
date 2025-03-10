8f19bc73ff50e165deeabdd7ad627a949522dd71
zanfooo <152177598+zanfooo@users.noreply.github.com>
2025-03-10 21:56:53 +0100

    Update Smartphone.java

--- NegoziElettronica/src/Smartphone.java
+++ NegoziElettronica/src/Smartphone.java
@@ -1,5 +1,24 @@
-public class Smartphone extends ProdottoElettronico{
+class Smartphone extends ProdottoElettronico {
     private String modello;
     private int memoria;
 
+    public Smartphone(String codice, String marca, double prezzo, String modello, int memoria) {
+        super(codice, marca, prezzo);
+        if (memoria <= 0) throw new IllegalArgumentException("La memoria deve essere maggiore di 0 GB.");
+        this.modello = modello;
+        this.memoria = memoria;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public int getMemoria() {
+        return memoria;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Modello: " + modello + ", Memoria: " + memoria + "GB";
+    }
 }
