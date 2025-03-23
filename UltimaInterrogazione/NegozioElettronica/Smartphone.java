4654442f2ec67b1d713e9c98255a58cb77f82fd6
zanfooo <152177598+zanfooo@users.noreply.github.com>
2025-03-23 15:34:57 +0100

    Create Smartphone.java

---
+++ UltimaInterrogazione/NegozioElettronica/Smartphone.java
@@ -0,0 +1,25 @@
+
+public class Smartphone extends ProdottoElettronico {
+
+    private String modello;
+    private int memoria;
+
+    public Smartphone(int codiceProdotto, String marca, int prezzo, String modello, int memoria) {
+        super(codiceProdotto, marca, prezzo);
+        this.modello = modello;
+        this.memoria = memoria;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public int getMemoria() {
+        return memoria;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Memoria: " + memoria;
+    }
+}
